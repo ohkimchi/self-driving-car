37e43ecc7a4a180a25ace3006b9a46f618055133
max <maxjunhuixu@gmail.com>
2020-03-07 21:41:15 +0800

    working

--- SearchAgent.py
+++ SearchAgent.py
@@ -1,4 +1,3 @@
-import heapq
 import sys
 from environment import Agent, Environment
 from searchUtils import searchUtils
@@ -14,24 +13,26 @@ def heuristic_cost_estimate(start, goal):
 
 
 def reconstruct_path(came_from, current):
-    total_path = []
-    while current in came_from:
-        content = came_from[current][-1]
-        total_path.append(content)
-        current = came_from[current]
-    total_path.append(current)
+
+    if current not in came_from:
+        total_path = [current]
+    else:
+        total_path = []
+        while current in came_from:
+            total_path.append(came_from[current][-1])
+            current = came_from[current][0]
+        total_path.append(current)
+
     ans = total_path[::-1][1:]
-    # print(ans)
     return ans
 
 
 def get_lowest_f_score(f_score):
     min_score = min(f_score.values())
-    return [(k, v) for k, v in f_score.items() if v == min_score][0]
+    return [(k, v) for k, v in f_score.items() if v == min_score]
 
 
 def a_star(start, goals):
-    print('start', start, 'goals', goals)
     start_tuple = start['location']
     actions = [None, 'forward-3x', 'forward-2x', 'forward', 'left', 'right']
     for goal in goals:
@@ -42,33 +43,33 @@ def a_star(start, goals):
         g_score = {start_tuple: 0}
         f_score = {start_tuple: heuristic_cost_estimate(start_tuple, goal_tuple)}
         current = start
-        current_tuple = start_tuple
+        current_tuple = start['location']
         while open_set:
             lowest_f_score = get_lowest_f_score(f_score)
-            current_tuple, _ = lowest_f_score
-            if current_tuple == goal_tuple:
-                return True, reconstruct_path(came_from, current_tuple)
-            open_set.remove(current_tuple)
-            del f_score[current_tuple]
-            closed_set.append(current_tuple)
-            for action in actions:
-                neighbor = the_env.applyAction(agent=the_agent, state=current, action=action)
-                prev = neighbor['previous']
-                prev = prev['location']
-                neighbor = neighbor['location']
-                if neighbor == prev:
-                    continue
-                neighbor_tuple = neighbor
-                distance = manhattan_distance(start_tuple, neighbor_tuple)
-                tentative_g_score = g_score[current_tuple] + distance
+            for current_tuple, _ in lowest_f_score:
+                if current_tuple == goal_tuple:
+                    return True, reconstruct_path(came_from, current_tuple)
+                open_set.remove(current_tuple)
+                del f_score[current_tuple]
+                closed_set.append(current_tuple)
+                for action in actions:
+                    neighbor = the_env.applyAction(agent=the_agent, state={'location': current_tuple}, action=action)
+                    prev = neighbor['previous']
+                    prev = prev['location']
+                    neighbor = neighbor['location']
+                    if neighbor == prev:
+                        continue
+                    neighbor_tuple = neighbor
+                    distance = manhattan_distance(current_tuple, neighbor_tuple)
+                    tentative_g_score = g_score[current_tuple] + distance
 
-                if neighbor_tuple not in open_set:
-                    open_set.append(neighbor_tuple)
-                elif tentative_g_score >= g_score[neighbor_tuple]:
-                    continue
-                came_from[neighbor] = (current_tuple, action)
-                g_score[neighbor_tuple] = tentative_g_score
-                f_score[neighbor_tuple] = g_score[neighbor_tuple] + heuristic_cost_estimate(neighbor_tuple, goal_tuple)
+                    if neighbor_tuple not in open_set:
+                        open_set.append(neighbor_tuple)
+                    elif tentative_g_score >= g_score[neighbor_tuple]:
+                        continue
+                    came_from[neighbor] = (current_tuple, action)
+                    g_score[neighbor_tuple] = tentative_g_score
+                    f_score[neighbor_tuple] = g_score[neighbor_tuple] + heuristic_cost_estimate(neighbor_tuple, goal_tuple)
 
         if current == goal:
             return True, reconstruct_path(came_from, current_tuple)
@@ -84,7 +85,6 @@ class SearchAgent(Agent):
         self.searchutil = searchUtils(env)
 
     def choose_action(self):
-        print(self.action_sequence)
         action = None
         if len(self.action_sequence) >= 1:
             action = self.action_sequence[0]
@@ -99,7 +99,6 @@ class SearchAgent(Agent):
         goal_states = [x for x in goal_states]
         """Write your algorithm for self driving car"""
         ok, act_sequence = a_star(start_state, goal_states)
-        print('act_sequence', act_sequence)
         return act_sequence
 
     def update(self):
@@ -110,6 +109,7 @@ class SearchAgent(Agent):
         goalstates = self.env.getGoalStates()
         inputs = self.env.sense(self)
         self.action_sequence = self.drive(goalstates, inputs)
+        print('self.action_sequence', self.action_sequence)
         action = self.choose_action()  # Choose an action
         self.state = self.env.act(self, action)
         return
